eda69bbb1d5080dccbaadb45b281ea0c5f0afc73
OziomaEunice <onwubikooziomaeunice@gmail.com>
2024-04-22 23:07:53 +0100

    Added main file, removed home files, other updates

---
+++ src/main.adb
@@ -0,0 +1,13 @@
+--  with Temperature; use Temperature;
+--  with Frige_And_Oven; use Frige_And_Oven;
+--  with Exterior_Interior_Areas; use Exterior_Interior_Areas;
+--  with Cm_Levels; use Cm_Levels;
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+
+procedure main is
+begin
+   -- print output in the console
+   Put_Line("Hi");
+end main;
