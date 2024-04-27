d92f90d53b787c925f7b1457197d0831414c51e3
OziomaEunice <onwubikooziomaeunice@gmail.com>
2024-04-27 17:15:45 +0100

    heating system updated

--- src/heating_system.ads
+++ src/heating_system.ads
@@ -1,12 +1,21 @@
 package Heating_System with SPARK_Mode is
 
-   -- Define the Temperature type
+   -- Define the Temperature and Window Status types
    type Temperature is range 0..40;
+   type Windows is (W_Open, W_Closed);
    
-   -- Function for the heating control system
-   procedure Control_Heating(Temp: in out Temperature; Windows_Open: out Boolean)
-     with
-       Pre => Temp = 15,
-       Post => (Temp >= 17 and Temp <= 19);
+   type Heating_Sys is record
+      T : Temperature;
+      W : Windows;
+   end record;
+   
+   
+   procedure Control_Temperature (Temp: in out Temperature) with
+     Pre => Temp = 15,
+     Post => (Temp >= 17 and Temp <= 19);
+   
+   procedure Windows_Status (Window: in out Windows) with
+     Pre => Window = W_Open,
+     Post => Window = W_Closed;
 
 end Heating_System;
